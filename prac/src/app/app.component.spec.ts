e36303256472703716d9589e97fdf8d3976fe0e4
vijay-kumar-icreon <vijay.kumar@icreon.com>
2017-08-14 15:27:20 +0530

    Simple Unit Test

--- prac/src/app/app.component.spec.ts
+++ prac/src/app/app.component.spec.ts
@@ -1,32 +1,45 @@
-import { TestBed, async } from '@angular/core/testing';
-
-import { AppComponent } from './app.component';
+import {TestBed, async} from '@angular/core/testing';
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
-
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                AppComponent
+            ],
+        }).compileComponents();
+    }));
+    it('should create the app', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    }));
+    it(`should have as title 'app'`, async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app.title).toEqual('app');
+    }));
+    it('should render title in a h1 tag', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
+    }));
+    describe('Compute Test', () => {
+        it('Zero Test', () => {
+            const appC = new AppComponent();
+            const res = appC.compute(0);
+            expect(res).toBe('ZERO');
+        })
+        it('Positive Test', () => {
+            const appC = new AppComponent();
+            const res = appC.compute(5);
+            expect(res).toBe('+ive');
+        })
+        it('Negative Test', () => {
+            const appC = new AppComponent();
+            const res = appC.compute(-9);
+            expect(res).toBe('-ive');
+        })
+    })
 });
--- prac/src/app/app.component.ts
+++ prac/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'app';
+
+    compute(num: number) {
+        if (num === 0) {
+            return 'ZERO';
+        } else if (num > 0) {
+            return '+ive';
+        } else {
+            return '-ive';
+        }
+    }
 }
